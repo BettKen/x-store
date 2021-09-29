64fc1e4d1b7b14735ce87aa2b62e1428aeb9172f
BettKen <bettken21@gmail.com>
2021-09-29 15:44:50 +0300

    new new commits

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req,res ){
-    return res.send('HELLO!');
+app.get('/', function (req, res) {
+  return res.send('HELLO!')
 })
 
-app.listen(3000, function(){
-    console.log('server running at port 3000 ')
+app.listen(3000, function () {
+  console.log('server running at port 3000 ')
 })
