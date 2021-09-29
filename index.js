beb778c904a68e3ab1a9fa767ed45bb999ea38b9
BettKen <bettken21@gmail.com>
2021-09-29 12:07:53 +0300

    Day 1

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req,res ){
+    return res.send('HELLO!');
+})
+
+app.listen(3000, function(){
+    console.log('server running at port 3000 ')
+})
